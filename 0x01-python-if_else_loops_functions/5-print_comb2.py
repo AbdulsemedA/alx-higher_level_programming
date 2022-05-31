b44e0c2824f648614d52c633e5597e62eadfde48
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2022-05-31 15:14:16 +0300

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 for number in range(100):
     if number < 10:
         print("{:d}{:d}".format(0, number), end=", ")
-    elif number >= 10 and number <= 99:
+    elif number >= 10 and number < 99:
         print("{}".format(number), end=", ")
 
