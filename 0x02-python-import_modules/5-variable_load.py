8e258146c19ddfad8eeaa594f639e09f57176f67
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2022-06-02 19:46:26 +0300

    Update 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import variable_load5 *
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
     print(a)
