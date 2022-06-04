8c27128b742dc0ce90c517748adaa8e0c1e24aa8
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2022-06-04 21:09:05 +0300

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/pyton3
 def new_in_list(my_list, idx, element):
-    if idx >= 0 and idx < len(my_list):
-        output = my_list[:]
-        output[idx] = element
-        return output
-    return my_list
+    """Replace element in a cpy list at specified pos."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+
+    cpy = [i for i in my_list]
+    cpy[idx] = element
+    return (cpy)
