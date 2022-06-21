ed8dc054e12de91e54601f9d7a24fd575af1085c
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2022-06-21 19:47:05 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,11 +4,19 @@
 
 
 class Square:
-    """ Instantiation"""
-    if type(size) is not int:
-        raise TypeError("size must be an integer")
-    if type < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
     def area(self):
-        return size * size
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
